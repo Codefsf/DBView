3833fc4517ad67d074141df8408ad3f476b29704
guoxudong01 <guoxudong01@corp.netease.com>
2022-02-18 16:24:10 +0800

    Initial Commit

---
+++ DBView/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  DBView
+//
+//  Created by bschenc on 2022/2/18.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ DBView/AppDelegate.m
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.m
+//  DBView
+//
+//  Created by bschenc on 2022/2/18.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (strong) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+
+@end
